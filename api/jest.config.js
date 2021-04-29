02f83a0d21dee9bf383c06c011282484e4f72953
Christopher Caldwell <1127514@iheartmedia.com>
2021-04-29 16:51:10 -0600

    idk

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
 	moduleFileExtensions: ["js"],
 	transform: {
-		"^.+\\.js?$": "babel-jest"
+		"^.+\\.js?$": "babel-jest",
 	},
 	transformIgnorePatterns: ["/node_modules/"],
 	moduleNameMapper: {
-		"^@/(.*)$": "<rootDir>/src/lambdas/$1"
+		"^@/(.*)$": "<rootDir>/src/lambdas/$1",
 	},
 	testMatch: [
-		"**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+		"**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)",
 	],
-	testURL: "http://localhost/"
+	testURL: "http://localhost/",
 };
